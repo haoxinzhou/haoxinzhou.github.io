7463be7d6cfa127cbbdb2772f13fadded8e26fe7
haoxinzhou <zhouhaoxin@gmail.com>
2025-11-23 08:24:03 +0000

    Deploying to gh-pages from @ haoxinzhou/haoxinzhou.github.io@d711af53f380391a2f1706a613b9cdf60aaca266 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,19 +405,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-cqed-quantum-sensing",
+          title: 'cQED quantum sensing',
           description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-cqed-quantum-sensing",
-          title: 'cQED Quantum Sensing',
-          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/cqed-sensing/";
             },},{id: "projects-electron-quantum-optics",
           title: 'Electron Quantum Optics',
-          description: "",
+          description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/electron-quantum-optics/";
             },},{id: "projects-quantum-devices-from-novel-materials",
